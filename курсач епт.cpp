8cff9b1506f64648388ba8d4472018cdc6c153d3
User54681 <anzolotylh@gmail.com>
2023-12-17 21:15:11 +0300

    сделала заготовки

--- курсач епт.cpp
+++ курсач епт.cpp
@@ -1,8 +1,60 @@
 ﻿#include <iostream>
+#include <vector>
 
+class Stack {
+private:
+    int value;
+    int *values;
+public:
+    Stack(int size);
+    ~Stack();
 
+    void push();
+    int pop();
+    int remove();
+    void print();
+    int size();
+    int copy();
+};
+
+Stack::Stack(int size) {
+    values = new int[size];
+}
+
+Stack::~Stack() {
+    delete[] values;
+}
+
+void Stack::push() {
+
+}
+
+int Stack::pop() {
+
+}
+
+int Stack::remove() {
+
+}
+
+void Stack::print() {
+
+}
+
+int Stack::size() {
+
+}
+
+int Stack::copy() {
+
+}
+
+class Node {
+
+};
 
 int main()
 {
-    
+    setlocale(LC_ALL, "Rus");
+    return 0;
 }
